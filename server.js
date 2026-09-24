59b874daab3a07276386da7442380c762e57f074
kushal-ramsinghani23 <kushal.ramsinghani23@gmail.com>
2026-09-25 04:35:19 +0530

    feat: integrate socket.io with express server

--- server.js
+++ server.js
@@ -1,18 +1,29 @@
 const express = require('express');
 const cors = require('cors');
+const http = require('http');
+const { Server } = require('socket.io');
+
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {origin: '*'}
+});
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('InvestSync API running');
-});
-
-const PORT = 3000;
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-});
-
 const companyRoutes = require('./routes/companyRoutes');
 app.use('/companies', companyRoutes);
+
+io.on('connection', (socket) => {
+    console.log(`Client connected: ${socket.id}`);
+
+    socket.on('disconnect', () => {
+        console.log(`Client disconnected: ${socket.id}`);
+    })
+})
+
+const PORT = 3000;
+server.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+})
